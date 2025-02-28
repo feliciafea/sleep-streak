467bded96bbda27258c40dc40dd13c43f5983a16
Carson Keavney <carsonkeavney@outlook.com>
2025-02-28 18:20:36 -0500

    Debloat and setup Prettier

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,52 +1,43 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Log In',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-          tabBarStyle: { display: 'none'},
-          href: null
+          tabBarStyle: { display: 'none' },
+          href: null,
         }}
       />
       <Tabs.Screen
-        name="home"
+        name="sleep"
         options={{
           title: 'Sleep',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="moon.fill" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="power-sleep"
+              size={24}
+              color={color}
+            />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="leaderboard"
         options={{
           title: 'Leaderboard',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="trophy.fill" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="leaderboard" size={24} color={color} />
+          ),
         }}
       />
     </Tabs>
---
+++ app/(tabs)/leaderboard.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Leaderboard screen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
