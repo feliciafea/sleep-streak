f063797eedf6b6dc86da991f44e1232a611f0c0d
Carson Keavney <carsonkeavney@outlook.com>
2025-02-28 19:23:47 -0500

    Adding react native firebase

--- app/(tabs)/sleep.tsx
+++ app/(tabs)/sleep.tsx
@@ -1,43 +1,7 @@
-import { useLocalSearchParams } from 'expo-router';
-import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
-import { FIRESTORE_DB } from '../../firebaseConfig';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
-import { useState } from 'react';
+import { Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
-  const db = FIRESTORE_DB;
-  const [funFact, setFact] = useState('');
-  const { email } = useLocalSearchParams();
-  const [currentFunFact, setCurrentFunFact] = useState('');
-  const [randomInfo, setRandomInfo] = useState<any>(null);
-
-  const saveFact = async (fact: string) => {
-    try {
-      const docRef = await addDoc(collection(db, 'funFacts'), {
-        fact: fact,
-        email: email,
-      });
-      console.log('Document written with ID: ', docRef.id);
-      setCurrentFunFact(fact);
-    } catch (e) {
-      console.error('Error adding document: ', e);
-    }
-  };
-
-  const getFact = async () => {
-    try {
-      const querySnapshot = await getDocs(collection(db, 'funFacts'));
-      const documents = querySnapshot.docs;
-      if (documents.length > 0) {
-        const randomIndex = Math.floor(Math.random() * documents.length);
-        console.log('Document data:', documents[randomIndex].data());
-        setRandomInfo(documents[randomIndex].data());
-      }
-    } catch (e) {
-      console.error('Error getting documents: ', e);
-    }
-  };
-
   return (
     <SafeAreaView>
       <Text>Sleep</Text>
@@ -45,4 +9,4 @@ export default function HomeScreen() {
   );
 }
 
-const styles = StyleSheet.create();
+const styles = StyleSheet.create({});
