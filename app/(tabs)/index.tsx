544e292700569dc6a9b551bf732a5721df36afde
Carson Keavney <carsonkeavney@outlook.com>
2025-03-02 18:53:55 -0500

    Very rough sleep tracking

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { getAuth, FirebaseAuthTypes } from '@react-native-firebase/auth';
 import { useEffect, useState } from 'react';
+import SleepTracker from '../../components/SleepTracker';
 
 export default function HomeScreen() {
   // Set an initializing state whilst Firebase connects
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState<FirebaseAuthTypes.User | null>();
+  const [initializing, setInitializing] = useState<boolean>(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
 
   function onAuthStateChanged(user: FirebaseAuthTypes.User | null) {
     setUser(user);
@@ -21,11 +22,33 @@ export default function HomeScreen() {
   if (initializing) return null;
 
   return (
-    <SafeAreaView>
-      <Text>Sleep</Text>
-      {user && <Text>User Email: {user.email}</Text>}
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#FFF' }}>
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Sleep Streak</Text>
+        {user && (
+          <>
+            <Text style={styles.welcomeText}>Welcome, {user.email}</Text>
+            <SleepTracker user={user} />
+          </>
+        )}
+      </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  welcomeText: {
+    fontSize: 16,
+    color: '#666',
+  },
+});
---
+++ utils/sleepTracking.ts
@@ -0,0 +1,128 @@
+import firestore, {
+  FirebaseFirestoreTypes,
+} from '@react-native-firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
+import { DeviceMotion } from 'expo-sensors';
+import delay from './delay';
+
+// Sleep Tracking Task that runs every 15 minutes and checks for movement
+// If movement is detected, it increases the penalty by 1
+TaskManager.defineTask('SLEEP_TRACKING_TASK', async () => {
+  const ACC_THRESHOLD = 0.2;
+  const ROT_THRESHOLD = 0.2;
+
+  let check = 0;
+
+  // First check for movement using DeviceMotion
+  DeviceMotion.setUpdateInterval(1000);
+  DeviceMotion.addListener((data) => {
+    if (!data.acceleration || !data.rotationRate)
+      return BackgroundFetch.BackgroundFetchResult.Failed;
+
+    const { x, y, z } = data.acceleration;
+    const { alpha, beta, gamma } = data.rotationRate;
+
+    const totalAcceleration = Math.sqrt(x * x + y * y + z * z);
+    const totalRotation = Math.sqrt(
+      alpha * alpha + beta * beta + gamma * gamma,
+    );
+
+    if (totalAcceleration > ACC_THRESHOLD || totalRotation > ROT_THRESHOLD) {
+      // If movement is detected, increase the penalty
+      check += 1;
+    }
+  });
+
+  // Wait for 5 seconds
+  await delay(5000);
+
+  // Update the penalty in AsyncStorage if check > 0
+  if (check > 0) {
+    const penalty = await AsyncStorage.getItem('sleepPenalty');
+    if (penalty) {
+      await AsyncStorage.setItem('sleepPenalty', String(Number(penalty) + 1));
+    } else {
+      return BackgroundFetch.BackgroundFetchResult.Failed;
+    }
+  }
+
+  return BackgroundFetch.BackgroundFetchResult.NewData;
+});
+
+export const startSleepSession = async (
+  userID: string,
+): Promise<FirebaseFirestoreTypes.DocumentSnapshot | null> => {
+  // Make sure they don't already have a session
+  const activeSession = await getActiveSleepSession(userID);
+  if (!activeSession?.empty) {
+    throw new Error('User already has an active sleep session');
+  }
+
+  // Make sure user can use Device Motion and Background Fetch
+  const motionPerm = await DeviceMotion.requestPermissionsAsync();
+  if (motionPerm.status !== 'granted') {
+    throw new Error('Motion permission not granted');
+  }
+
+  // Create sleepSession document in Firestore
+  const sleep = await firestore().collection('sleepSessions').add({
+    userID: userID,
+    penalty: 0,
+    startTime: firestore.FieldValue.serverTimestamp(),
+    active: true,
+  });
+
+  // Save the session ID and sleepPenalty in AsyncStorage
+  await AsyncStorage.setItem('sleepSessionID', sleep.id);
+  await AsyncStorage.setItem('sleepPenalty', String(0));
+
+  // Start background task that runs every 15 minutes
+  await BackgroundFetch.registerTaskAsync('SLEEP_TRACKING_TASK', {
+    minimumInterval: 15 * 60, // 15 minutes
+    stopOnTerminate: false,
+    startOnBoot: false,
+  });
+
+  return await sleep.get();
+};
+
+export const stopSleepSession = async (
+  userID: string,
+): Promise<FirebaseFirestoreTypes.DocumentSnapshot | null> => {
+  // Stop the background task
+  await BackgroundFetch.unregisterTaskAsync('SLEEP_TRACKING_TASK');
+
+  // Get the sleep session ID from AsyncStorage
+  const sleepSessionID = await AsyncStorage.getItem('sleepSessionID');
+  if (!sleepSessionID) {
+    throw new Error('No active sleep session found');
+  }
+
+  // Update the sleep session document in Firestore
+  const docRef = firestore().collection('sleepSessions').doc(sleepSessionID);
+  await docRef.update({
+    endTime: firestore.FieldValue.serverTimestamp(),
+    active: false,
+  });
+
+  // Clear the session ID and penalty from AsyncStorage
+  await AsyncStorage.removeItem('sleepSessionID');
+  await AsyncStorage.removeItem('sleepPenalty');
+
+  // Return the updated document
+  return await docRef.get();
+};
+
+export const getActiveSleepSession = async (
+  userID: string,
+): Promise<FirebaseFirestoreTypes.QuerySnapshot | null> => {
+  return await firestore()
+    .collection('sleepSessions')
+    .where('userID', '==', userID)
+    .where('active', '==', true)
+    .orderBy('startTime', 'desc')
+    .limit(1)
+    .get();
+};
